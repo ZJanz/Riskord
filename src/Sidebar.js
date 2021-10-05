3e3d6aec040c5523d82cfa457e24a8963810cd2d
ZJanz <ZACHATTACK100000@gmail.com>
2021-10-05 19:54:22 -0400

    ExpandMoreIcon

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <h2>Hello, I am the sidebar</h2>
+            <div className="sidebar_top">
+                <h3>Clever Programmer</h3>
+                <ExpandMoreIcon />
+            </div>
         </div>
     )
 }
