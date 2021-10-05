d4260ba412c5e6adaf47b4da2ec37aabb3294df2
ZJanz <ZACHATTACK100000@gmail.com>
2021-10-05 16:52:03 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+function App() {    
+     return (
+      <div className="app">
+      <h2>Let's Build a Discord Clone!!!</h2>
+      </div>
+  );
+}
+export default App;
